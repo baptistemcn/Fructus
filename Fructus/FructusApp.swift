33942319fa5c7d310644360c37ab068d1e2a4ea2
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-18 19:14:41 +0200

    init ✨

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Baptiste on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
