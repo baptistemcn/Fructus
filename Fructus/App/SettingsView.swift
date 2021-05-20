cfca726b1a26bb1158df3b38f7ea12a558a197c9
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-20 18:30:46 +0200

    feat: Setting panel pt.1

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -18,7 +18,27 @@ struct SettingsView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    // MARK: - SECTION 1
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterols. Fruits are sources of many essentil nutrients, including potassium, diatery fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
+                    }
+                    
+                    // MARK: - SECTION 2
+                    
+                    // MARK: - SECTION 3
+                    
                 } //: VStack
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(trailing: Button(action: {
